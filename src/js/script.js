25bbe7bc7bf7ca30e720629800d27829dfc1ed66
Dima <dima.agstivjobs@gmail.com>
2021-10-27 23:46:04 +0300

    add page up icon and script also  animation beatheart

--- src/js/script.js
+++ src/js/script.js
@@ -82,4 +82,40 @@ $(document).ready(function(){
 
    $('input[name=phone]').mask("+38 (999) 999-99-99");
 
+   $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 1200) {
+        $('.pageup').fadeIn();
+     } else {
+        $('.pageup').fadeOut();
+     }
+  });
+
+  $("a[href*=#]").on("click", function(e){
+   var anchor = $(this);
+   $('html, body').stop().animate({
+     scrollTop: $(anchor.attr('href')).offset().top
+   }, 777);
+   e.preventDefault();
+   return false;
  });
+
+});
